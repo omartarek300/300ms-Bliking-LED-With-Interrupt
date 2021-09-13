9af39b22f80ee3f343d8d5d448620c6719c4ce86
Omar Tarek <omar.t4352@gmail.com>
2021-09-13 03:26:48 +0200

    quality edited

---
+++ 300ms-Bliking-LED-With-Interrupt/300ms-Bliking-LED-With-Interrupt/Debug/MCAL/timer.d
@@ -0,0 +1,18 @@
+MCAL/timer.d MCAL/timer.o: ../MCAL/timer.c ../MCAL/timer.h \
+ ../MCAL/../bit_operations.h ../MCAL/../MC/My_avr.h \
+ ../MCAL/../MC/../types.h ../MCAL/../types.h ../MCAL/../MC/Interrupts.h \
+ ../MCAL/../MC/../bit_operations.h
+
+../MCAL/timer.h:
+
+../MCAL/../bit_operations.h:
+
+../MCAL/../MC/My_avr.h:
+
+../MCAL/../MC/../types.h:
+
+../MCAL/../types.h:
+
+../MCAL/../MC/Interrupts.h:
+
+../MCAL/../MC/../bit_operations.h:
---
+++ 300ms-Bliking-LED-With-Interrupt/300ms-Bliking-LED-With-Interrupt/main.c
@@ -0,0 +1,21 @@
+/*
+ * 300ms_Blinking_LED.c
+ *
+ * Created: 9/7/2021 2:22:07 AM
+ * Author : Omar
+ */ 
+#include "ECUAL/LED.h"
+#define counts_num 293
+int main(void)
+{			
+    LED_init(PA,pin1);
+	config conf = {timer1, compare, f_cpu_clk_1024, counts_num}; // initialize timer
+	Timer_init(&conf);
+	Timer_setCallBack(timer1, LED_toggle);
+	SETBIT(SREG,bit7); //ENABLE GLOBAL INTERRUPT
+    while (1) 
+    {
+		
+    }
+}
+
